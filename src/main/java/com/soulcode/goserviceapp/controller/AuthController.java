6242ef7babca45672c8bce474d2eb54ff72a06fa
CarlosEduardoMendesPereira <carloseduardomendes201@gmail.com>
2023-08-21 17:05:29 -0300

    page serviços e page usuarios

--- src/main/java/com/soulcode/goserviceapp/controller/AuthController.java
+++ src/main/java/com/soulcode/goserviceapp/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.soulcode.goserviceapp.controller;
 
-import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
